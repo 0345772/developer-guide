acec74d9b1222e977e843af46f3ec7c6c738bba8
step_65 <0345772@ukr.net>
2022-12-05 15:30:27 +0200

    Style organization. Adding themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import { Suspense } from 'react';
+import { Suspense, useContext, useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
+
+
 
 export const App = () => {
+  const {theme, toggleTheme} = useTheme()
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>theme</button>
             <Link to={"/main"}>Главная</Link>
             <Link to={"/about"}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                <Route path={"main"} element={<MainPageAsync />} />
-                <Route path={"about"} element={<AboutPageAsync />} />
-            </Routes>
+                <Routes>
+                    <Route path={"main"} element={<MainPageAsync />} />
+                    <Route path={"about"} element={<AboutPageAsync />} />
+                </Routes>
             </Suspense>
         </div>
     );
