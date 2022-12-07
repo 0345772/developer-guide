44b640531e282883ced4a678fdc821da00fdf9e9
step_65 <0345772@ukr.net>
2022-12-07 22:16:03 +0200

    add Sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,4 @@
-import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { AppLink } from 'shared/ui/AppLink/AppLink';
 import cls from "./Navbar.module.scss";
 
@@ -11,7 +9,6 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-          <ThemeSwitcher/>
             <div className={cls.links}>
                 <AppLink to={"/"} className={cls.mainLink}>Главная</AppLink>
                 <AppLink to={"/about"} className={cls.aboutLink}>О сайте</AppLink>
