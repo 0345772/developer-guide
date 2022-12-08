35e497ac5d4ea644b12e68574dbaf0d82e8bd7f9
step_65 <0345772@ukr.net>
2022-12-08 14:23:29 +0200

    add 118n

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,8 +1,11 @@
-const  AboutPage = () => {
+import { useTranslation } from "react-i18next";
+
+const AboutPage = () => {
+    const { t, i18n } = useTranslation('about');
     return (
-        <div className='test'>
-            <h1>About Page</h1>
+        <div className="test">
+            <h1>{t("О сайте")}</h1>
         </div>
     );
-}
+};
 export default AboutPage;
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,8 +1,10 @@
+import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
+  const {t, i18n} = useTranslation('main')
   return(
     <div className='test'>
-    <h1>Main Page</h1>
+    <h1>{t('Главная страница')}</h1>
   </div>
   )
 }
