cd1806b1f714961f9bb843449c342c8a84d4121e
step_65 <0345772@ukr.net>
2022-12-10 13:19:21 +0200

    add Stylelint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,15 @@
-import './styles/index.scss';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar';
-import { Suspense } from 'react';
-import { AppRouter } from './providers/router';
-import { useTheme } from './providers/ThemeProvider';
+import "./styles/index.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
+import { Suspense } from "react";
+import { AppRouter } from "./providers/router";
+import { useTheme } from "./providers/ThemeProvider";
 
 export const App = () => {
     const { theme } = useTheme();
     return (
-        <div className={classNames('app', {}, [theme])}>
+        <div className={classNames("app", {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
