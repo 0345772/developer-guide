624b6c6e41456e5a16cc89c1bcf004cce6670f2d
step_65 <0345772@ukr.net>
2022-12-20 23:15:45 +0200

    update end approve storybok-scrins

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,10 @@
 import { useTranslation } from 'react-i18next';
 
-import { BugButton } from 'app/providers/ErrorBoundary';
-
 const MainPage = () => {
     const { t, i18n } = useTranslation('main');
     return (
         <div>
             <h1>{t('Главная страница')}</h1>
-            <BugButton />
         </div>
     );
 };
