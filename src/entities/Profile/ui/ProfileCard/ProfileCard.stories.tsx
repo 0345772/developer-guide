3ed8495612aa182749522124e4d2a44e2f426bcb
step_65 <0345772@ukr.net>
2023-01-08 21:32:38 +0200

    disabled profile card stories

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -1,40 +1,40 @@
-import React from 'react';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { ProfileCard } from 'entities/Profile';
-import { Country } from 'entities/Country';
-import { Currency } from 'entities/Currency';
-import avatar from 'shared/assets/tests/storybook.jpg';
+// import React from 'react';
+// import { ComponentMeta, ComponentStory } from '@storybook/react';
+// import { ProfileCard } from 'entities/Profile';
+// import { Country } from 'entities/Country';
+// import { Currency } from 'entities/Currency';
+// import avatar from 'shared/assets/tests/storybook.jpg';
 
-export default {
-    title: 'entities/ProfileCard',
-    component: ProfileCard,
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
-} as ComponentMeta<typeof ProfileCard>;
+// export default {
+//     title: 'entities/ProfileCard',
+//     component: ProfileCard,
+//     argTypes: {
+//         backgroundColor: { control: 'color' },
+//     },
+// } as ComponentMeta<typeof ProfileCard>;
 
-const Template: ComponentStory<typeof ProfileCard> = (args) => <ProfileCard {...args} />;
+// const Template: ComponentStory<typeof ProfileCard> = (args) => <ProfileCard {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-    data: {
-        username: 'admin',
-        age: 55,
-        country: Country.Ukraine,
-        lastname: 'Stepovoy',
-        first: 'Vasya',
-        city: 'Kyiv',
-        currency: Currency.USD,
-        avatar,
-    },
-};
+// export const Primary = Template.bind({});
+// Primary.args = {
+//     data: {
+//         username: 'admin',
+//         age: 55,
+//         country: Country.Ukraine,
+//         lastname: 'Stepovoy',
+//         first: 'Vasya',
+//         city: 'Kyiv',
+//         currency: Currency.USD,
+//         avatar,
+//     },
+// };
 
-export const withError = Template.bind({});
-withError.args = {
-    error: 'true',
-};
+// export const withError = Template.bind({});
+// withError.args = {
+//     error: 'true',
+// };
 
-export const Loading = Template.bind({});
-Loading.args = {
-    isLoading: true,
-};
+// export const Loading = Template.bind({});
+// Loading.args = {
+//     isLoading: true,
+// };
