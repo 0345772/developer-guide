eba142d82ed7eca27a05c8612d6d169e4d415505
step_65 <0345772@ukr.net>
2023-01-11 19:09:29 +0200

    add refrech plugin, create template config

---
+++ src/pages/TestPage/MyTest1/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const MyTest1 = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+MyTest1.propTypes = {
+
+};
+
+export default MyTest1;
---
+++ src/pages/TestPage/TestsComponent/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const TestsComponent = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+TestsComponent.propTypes = {
+
+};
+
+export default TestsComponent;
