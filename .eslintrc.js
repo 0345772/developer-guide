910841ece05facea05c07314de32b470c48d6d81
step_65 <0345772@ukr.net>
2023-04-10 17:21:15 +0300

    update custom eslint plugin, add support for aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -80,7 +80,7 @@ module.exports = {
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
-        'step65-plugin/path-checker': 'error',
+        'step65-plugin/path-checker': ['error', {alias: '@'}],
     },
     globals: {
         __IS_DEV__: true,
