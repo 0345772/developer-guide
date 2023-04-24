aa1e864275534f7a2f30a4e38f4c7645c92936b3
step_65 <0345772@ukr.net>
2023-04-24 17:07:52 +0300

    add helpers for slice, selectors, actions

--- src/entities/Counter/ui/Counter/Counter.tsx
+++ src/entities/Counter/ui/Counter/Counter.tsx
@@ -1,41 +1,54 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@/shared/ui/Button';
-import { counterActions } from '../../model/slice/counterSlice';
-import { getCounterValue } from '../../model/selectors/getCounterValue/getCounterValue';
+import {
+    useCounterActions,
+} from '../../model/slice/counterSlice';
+import {
+    useCounterValue,
+} from '../../model/selectors/getCounterValue/getCounterValue';
 
 export const Counter = () => {
     const dispatch = useDispatch();
-    const counterValue = useSelector(getCounterValue);
+    const counterValue = useCounterValue();
     const { t } = useTranslation();
+    const { decrement, increment, add } = useCounterActions();
 
-    const increment = () => {
-        dispatch(counterActions.increment());
+    const handleInc = () => {
+        increment();
     };
 
-    const decrement = () => {
-        dispatch(counterActions.decrement());
+    const handleDec = () => {
+        decrement();
+    };
+
+    const handleAddFive = () => {
+        add(5);
     };
 
     return (
-        <div>
-            <h1 data-testid="value-title">
+        <div style={{ margin: '50px' }}>
+            <h3 style={{ textAlign: 'center' }} data-testid="value-title">
                 {t('Значение')}
                 :
-                {counterValue}
-            </h1>
-            <Button
-                onClick={increment}
-                data-testid="increment-btn"
-            >
-                {t('прибавить')}
-            </Button>
-            <Button
-                data-testid="decrement-btn"
-                onClick={decrement}
-            >
+                <span
+                    style={{
+                        fontSize: '45px',
+                        color: 'red',
+                        padding: '2px 5px',
+                        marginLeft: '10px',
+                    }}
+                >
+                    {counterValue}
+                </span>
+            </h3>
+            <Button data-testid="decrement-btn" onClick={handleDec}>
                 {t('отнять')}
             </Button>
+            <Button onClick={handleInc} data-testid="increment-btn">
+                {t('прибавить')}
+            </Button>
+            <Button onClick={handleAddFive}>{t('прибавить 5')}</Button>
         </div>
     );
 };
--- src/pages/MainPage/ui/MainPage/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage/MainPage.tsx
@@ -3,6 +3,7 @@ import { memo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
 import { RatingCard } from '@/entities/Rating';
+import { Counter } from '@/entities/Counter';
 
 const MainPage = memo(() => {
     const { t, i18n } = useTranslation('main');
@@ -13,6 +14,7 @@ const MainPage = memo(() => {
     return (
         <Page>
             <h1>{t('Главная страница')}</h1>
+            <Counter />
             <RatingCard
                 title="Как Вам статья?"
                 feedbackTitle="Оставьвте отзыв о статье"
