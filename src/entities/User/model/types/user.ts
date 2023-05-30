4f27f5965c44841b291312252b16404096424dd4
step_65 <0345772@ukr.net>
2023-05-30 17:38:38 +0300

    add feature flags concept

--- src/entities/User/model/types/user.ts
+++ src/entities/User/model/types/user.ts
@@ -1,15 +1,17 @@
-import { UserRole } from '../consts/userConsts';
+import { FeatureFlags } from "@/shared/types/featureFlags";
+import { UserRole } from "../consts/userConsts";
 
 export interface User {
-    id: string;
-    username: string;
-    avatar?: string;
-    roles?: UserRole[];
+  id: string;
+  username: string;
+  avatar?: string;
+  roles?: UserRole[];
+  features?: FeatureFlags;
 }
 
 export interface UserSchema {
-    authData?: User;
-    _inited: boolean;
+  authData?: User;
+  _inited: boolean;
 }
 
 export { UserRole };
---
+++ src/shared/lib/features/setGetFeatures.ts
@@ -0,0 +1,16 @@
+import { FeatureFlags } from "@/shared/types/featureFlags";
+
+// ! Фичи в ходе сессии не меняются
+// ? Именно поэтому их не обязательно делать рефктивными
+
+let featureFlags: FeatureFlags;
+
+export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
+  if (newFeatureFlags) {
+    featureFlags = newFeatureFlags;
+  }
+}
+
+export function getFeatureFlag(flag: keyof FeatureFlags) {
+  return featureFlags[flag];
+}
