b0a84a62f1e98ff23ed6b6aeb57fd34414c9bd90
step_65 <0345772@ukr.net>
2023-06-20 20:24:46 +0300

    add toggle feature for components

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,6 +62,7 @@ module.exports = {
           "definition",
           "gap",
           "as",
+          "feature",
         ],
       },
     ],
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -4,8 +4,8 @@ import { useParams } from 'react-router-dom';
 import { ArticleDetails } from '@/entities/Article';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import {
-    DynamicModuleLoader,
-    ReducersList,
+  DynamicModuleLoader,
+  ReducersList,
 } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { Page } from '@/widgets/Page';
 import { VStack } from '@/shared/ui/Stack';
@@ -15,59 +15,60 @@ import { articleDetailsPageReducer } from '../../model/slice';
 import { ArticleDetailsPageHeader } from '../ArticleDetailsPageHeader/ArticleDetailsPageHeader';
 import { ArticleDetailsComments } from '../ArticleDetailsComments/ArticleDetailsComments';
 import { ArticleRating } from '@/features/articleRating';
-import { getFeatureFlag, toggleFeatures } from '@/shared/lib/features';
+import {
+  ToggleFeatures,
+  getFeatureFlag,
+  toggleFeatures,
+} from '@/shared/lib/features';
 import { Counter } from '@/entities/Counter';
 import { Card } from '@/shared/ui/Card';
 
 interface ArticleDetailsPageProps {
-    className?: string;
+  className?: string;
 }
 
 const reducers: ReducersList = {
-    articleDetailsPage: articleDetailsPageReducer,
+  articleDetailsPage: articleDetailsPageReducer,
 };
 
 const CounterRedesigned = () => (
-    <div>
-        <h5>new Counter</h5>
-    </div>
+  <div>
+    <h5>new Counter</h5>
+  </div>
 );
 
 const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
-    const { t } = useTranslation('article-details');
-    const { id } = useParams<{ id: string }>();
+  const { t } = useTranslation('article-details');
+  const { id } = useParams<{ id: string }>();
 
-    if (!id) {
-        return null;
-    }
+  if (!id) {
+    return null;
+  }
 
-    const articleRatingCard = toggleFeatures({
-        name: 'isArticleRatingEnabled',
-        on: () => <ArticleRating articleId={id} />,
-        off: () => (
-            <Card>
+  return (
+    <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+      <Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+        <VStack gap="16" max>
+          <ArticleDetailsPageHeader />
+          <ArticleDetails id={id} />
+          {/* {articleRatingCard} */}
+          <ToggleFeatures
+            feature={'isArticleRatingEnabled'}
+            on={<ArticleRating articleId={id} />}
+            off={
+              <Card>
                 {t(
-                    'Для данного пользователя просмотр и оценка статьи не доступна',
+                  'Для данного пользователя просмотр и оценка статьи не доступна',
                 )}
-            </Card>
-        ),
-    });
-
-    return (
-        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-            <Page
-                className={classNames(cls.ArticleDetailsPage, {}, [className])}
-            >
-                <VStack gap="16" max>
-                    <ArticleDetailsPageHeader />
-                    <ArticleDetails id={id} />
-                    {articleRatingCard}
-                    <ArticleRecommendationsList />
-                    <ArticleDetailsComments id={id} />
-                </VStack>
-            </Page>
-        </DynamicModuleLoader>
-    );
+              </Card>
+            }
+          />
+          <ArticleRecommendationsList />
+          <ArticleDetailsComments id={id} />
+        </VStack>
+      </Page>
+    </DynamicModuleLoader>
+  );
 };
 
 export default memo(ArticleDetailsPage);
