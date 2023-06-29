fe7a988c8d24928aaf81a4da9622e88a71479803
step_65 <0345772@ukr.net>
2023-06-29 17:27:11 +0300

    fix icons size and color

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,42 +1,59 @@
-import webpack from 'webpack';
-import { BuildOptions } from './types/config';
-import { buildCssLoader } from './loaders/buildCssLoader';
-import { buildBabelLoader } from './loaders/buildBabelLoader';
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+import { buildCssLoader } from "./loaders/buildCssLoader";
+import { buildBabelLoader } from "./loaders/buildBabelLoader";
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-    const { isDev } = options;
-    const svgLoader = {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-    };
+  const { isDev } = options;
+  const svgLoader = {
+    test: /\.svg$/,
+    use: [
+      {
+        loader: "@svgr/webpack",
+        options: {
+          icon: true,
+          svgoConfig: {
+            plugins: [
+              {
+                name: "convertColors",
+                params: {
+                  currentColor: true,
+                },
+              },
+            ],
+          },
+        },
+      },
+    ],
+  };
 
-    const codeBabelLoader = buildBabelLoader({ ...options, isTsx: false });
-    const tsxCodeBabelLoader = buildBabelLoader({ ...options, isTsx: true });
+  const codeBabelLoader = buildBabelLoader({ ...options, isTsx: false });
+  const tsxCodeBabelLoader = buildBabelLoader({ ...options, isTsx: true });
 
-    const cssLoader = buildCssLoader(isDev);
+  const cssLoader = buildCssLoader(isDev);
 
-    // Если не используем тайпскрипт - нужен babel-loader
-    // const typescriptLoader = {
-    //     test: /\.tsx?$/,
-    //     use: 'ts-loader',
-    //     exclude: /node_modules/,
-    // };
+  // Если не используем тайпскрипт - нужен babel-loader
+  // const typescriptLoader = {
+  //     test: /\.tsx?$/,
+  //     use: 'ts-loader',
+  //     exclude: /node_modules/,
+  // };
 
-    const fileLoader = {
-        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
-        use: [
-            {
-                loader: 'file-loader',
-            },
-        ],
-    };
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
 
-    return [
-        fileLoader,
-        svgLoader,
-        codeBabelLoader,
-        tsxCodeBabelLoader,
-        // typescriptLoader,
-        cssLoader,
-    ];
+  return [
+    fileLoader,
+    svgLoader,
+    codeBabelLoader,
+    tsxCodeBabelLoader,
+    // typescriptLoader,
+    cssLoader,
+  ];
 }
